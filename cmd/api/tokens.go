43b8b5c33006a49fe84091f59d43278583436782
Siraj <sgsdbs@gmail.com>
2021-09-06 22:07:24 +0300

    Добавил тестовую jwt авторизацию

---
+++ cmd/api/tokens.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/pascaldekloe/jwt"
+	"go-movies-backend/models"
+	"golang.org/x/crypto/bcrypt"
+	"net/http"
+	"time"
+)
+
+var validUser = models.User{
+	ID:       10,
+	Email:    "siraj@here.com",
+	Password: "$2a$12$qAvfYeUGBkqNzUlIrnz5jOuW4bGdgHHP.14UcEkx2A05CIhNNVCZO",
+}
+
+type Credentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+// Signin Пока что фейковая авторизация для проверки
+func (app *application) Signin(w http.ResponseWriter, r *http.Request) {
+	var creds Credentials
+
+	err := json.NewDecoder(r.Body).Decode(&creds)
+	if err != nil {
+		app.errorJSON(w, errors.New("unauthorized"))
+		return
+	}
+
+	hashedPassword := validUser.Password
+
+	err = bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(creds.Password))
+	if err != nil {
+		app.errorJSON(w, errors.New("unauthorized"))
+		return
+	}
+
+	var claims jwt.Claims
+	claims.Subject = fmt.Sprint(validUser.ID)
+	claims.Issued = jwt.NewNumericTime(time.Now())
+	claims.NotBefore = jwt.NewNumericTime(time.Now())
+	claims.Expires = jwt.NewNumericTime(time.Now().Add(24 * time.Hour))
+	claims.Issuer = "mydomain.com"
+	claims.Audiences = []string{"mydomain.com"}
+
+	jwtBytes, err := claims.HMACSign(jwt.HS256, []byte(app.config.jwt.secret))
+	if err != nil {
+		app.errorJSON(w, errors.New("error signing"))
+		return
+	}
+
+	app.writeJSON(w, http.StatusOK, jwtBytes, "response")
+
+}
